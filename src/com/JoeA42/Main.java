4077519c61a569a5923de8699330c8ecc50baf94
JoeA42 <36778393+JoeA42@users.noreply.github.com>
2020-03-26 11:25:37 -0600

    Renamed parent folder added libraries

--- src/com/JoeA42/Main.java
+++ src/com/JoeA42/Main.java
@@ -1,11 +1,42 @@
 package com.JoeA42;
 
-public class Main {
+public class Main implements Runnable{
 
     private int width = 1280;
     private int height = 720;
 
+    private Thread thread;
+    private boolean running = false;
+
+    public void start(){
+        running = false;
+        thread = new Thread(this, "Game");
+        thread.start();
+    }
+
+    private void init(){
+
+
+    }
+
+    public void run(){
+        init();
+        while (running){
+            update();
+            render();
+        }
+
+    }
+
+    private void update(){
+
+    }
+
+    private void render(){
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        new Main().start();
     }
 }
