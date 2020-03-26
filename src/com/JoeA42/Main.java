c7b8f23a007b98d805b7152ca855a9182dadc127
JoeA42 <36778393+JoeA42@users.noreply.github.com>
2020-03-26 10:40:18 -0600

    Adjusted folder hierarchy

--- src/com/JoeA42/Main.java
+++ src/com/JoeA42/Main.java
@@ -1,6 +1,9 @@
-package Main;
+package com.JoeA42;
 
 public class Main {
+
+    private int width = 1280;
+    private int height = 720;
 
     public static void main(String[] args) {
 	// write your code here
